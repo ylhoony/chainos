b78d0739a0916e8efd0a37d29860a5f28d9e1ef5
Hoon Lee <yl.hoony@gmail.com>
2018-05-02 19:29:22 -0700

    set routes and added dummy contents

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  get "/dashboard", to: "navigations#dashboard", as: "nav_dashboard"
+  get "/demand", to: "navigations#demand", as: "nav_demand"
+  get "/supply", to: "navigations#supply", as: "nav_supply"
   
   root to: "navigations#index"
   
