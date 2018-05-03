ab7bc0767230d9331dcf11ec3e24de72e20844bd
Hoon Lee <yl.hoony@gmail.com>
2018-05-03 16:33:31 -0700

    added routes

--- app/controllers/countries_controller.rb
+++ app/controllers/countries_controller.rb
@@ -1,2 +1,33 @@
 class CountriesController < ApplicationController
+
+  def index
+  
+  end
+
+  def new
+    
+  end
+
+  def create
+    
+  end
+
+  def show
+    
+  end
+
+  def update
+    
+  end
+
+  def delete
+    
+  end
+
+  private
+
+  def country_params
+    # params.require(:country).permit(:name, alpha_2_code, alpha_3_code, numeric_code, :status)
+  end
+
 end
