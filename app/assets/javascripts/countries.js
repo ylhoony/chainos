2e520d9d8010a4701bc78c6c7e521292a3ed2b60
Hoon Lee <yl.hoony@gmail.com>
2018-05-05 16:55:17 -0700

    added getCountries function

--- app/assets/javascripts/countries.js
+++ app/assets/javascripts/countries.js
@@ -10,6 +10,21 @@ $(".update i").click(function(e){
   console.log($(e.target).data());
 });
 
+function getCountriesList() {
+  fetch('/countries.json')
+  .then(res => res.json())
+  .then(data => console.log(data))
+}
+
+function handlebarSetup() {
+
+}
+
+function init() {
+  // handlebarSetup();
+  getCountriesList();
+}
+
 document.addEventListener("DOMContentLoaded", function(event) {
-  console.log("hello, DOM content load!")
+  init();
 });
