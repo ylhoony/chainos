d3fdd8b654baf48abf54880d3d4aeb8081685bde
Hoon Lee <yl.hoony@gmail.com>
2018-05-06 00:51:05 -0700

    set click event inside displayCountriesList function

--- app/assets/javascripts/countries.js
+++ app/assets/javascripts/countries.js
@@ -1,19 +1,22 @@
-$(".create button").click(function() {
-  console.log("create!");
-});
-
-$(".delete button").click(function(e){
-  console.log(e.target);
-});
-
-$(".update button").click(function(e){
-  console.log($(e.target).data());
-});
-
 function displayCountriesList(data) {
   const template = Handlebars.compile(document.getElementById("country-template").innerHTML);
   const list = { countries: data };
   document.getElementById("content-table").innerHTML = template(list);
+
+  $(".update button").click(function(e){
+    console.log("update", e.target);
+  });
+
+  $(".delete button").click(function(e){
+    console.log("delete", e.target);
+  });
+
+  $(".input-text").on("click", function(e){
+    // reset();
+    // $( "input[type='text']" ).prop( "disabled", true);
+    // $(".input-text").prop("disabled", true);
+    $(e.target).prop("disabled", false);
+  })
 }
 
 function getCountriesList() {
@@ -35,6 +38,8 @@ document.addEventListener("DOMContentLoaded", function(event) {
   init();
 });
 
-// $(function() {
-//   init();
-// })
+$(function() {
+  $(".create button").click(function() {
+    console.log("create!");
+  });
+})
