f7e6b2fbaa4b10cb6fcdc8ad763e97e4c3c0a9f7
Hoon Lee <yl.hoony@gmail.com>
2018-05-07 10:39:06 -0700

    added ajax to create a new user

--- app/assets/javascripts/devise/registrations.js
+++ app/assets/javascripts/devise/registrations.js
@@ -1,4 +1,24 @@
+$(function(){
 
-$('#signup-form button').click(function() {
-  console.log("button working")
+  $('form#new_user').submit(function(e) {
+    e.preventDefault();
+    // console.log("button working")
+    const $form = $(this);
+    const action = $form.attr("action");
+    const params = $form.serialize();
+
+    $.ajax({
+      url: action,
+      method: "post",
+      data: params,
+      dataType: "json"
+    })
+    .done((res) => {
+      console.log(res);
+    })
+    .fail((err) => {
+      alert( "error" );
+    })
+  });
+
 });
