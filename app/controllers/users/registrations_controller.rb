b06fc7ac5c33fac0ff37f0eb032f7fa605a5d03a
Hoon Lee <yl.hoony@gmail.com>
2018-05-07 11:34:28 -0700

    updated respond format on custom devise controller

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -11,8 +11,6 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # POST /resource
   def create
-    # binding.pry
-    # super
     build_resource(sign_up_params)
 
     resource.save
@@ -21,7 +19,11 @@ class Users::RegistrationsController < Devise::RegistrationsController
       if resource.active_for_authentication?
         set_flash_message! :notice, :signed_up
         sign_up(resource_name, resource)
-        respond_with resource, location: after_sign_up_path_for(resource)
+        # respond_with resource, location: after_sign_up_path_for(resource)
+        respond_to do |format|
+          format.html { redirect_to after_sign_up_path_for(resource) }
+          format.json { render json: resource, status: 201 }
+        end
       else
         set_flash_message! :notice, :"signed_up_but_#{resource.inactive_message}"
         expire_data_after_sign_in!
