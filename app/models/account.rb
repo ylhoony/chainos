63a934ce39950268d4aa585be652a86a265721b2
Hoon Lee <yl.hoony@gmail.com>
2018-05-08 11:29:34 -0700

    updated association

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,2 +1,4 @@
 class Account < ApplicationRecord
+  has_many :employees
+  has_many :users, through: :employees
 end
--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,2 +1,4 @@
 class Employee < ApplicationRecord
+  belongs_to :account
+  belongs_to :user
 end
