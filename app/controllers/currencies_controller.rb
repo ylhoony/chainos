2a7183b90a53a39f6b72ef8d661fce072606e115
Hoon Lee <yl.hoony@gmail.com>
2018-05-08 12:52:46 -0700

    set routes for currencies

--- app/controllers/currencies_controller.rb
+++ app/controllers/currencies_controller.rb
@@ -1,2 +1,61 @@
 class CurrenciesController < ApplicationController
+
+  def index
+    @currencies = Currency.all
+    respond_to do |format|
+      format.html { render :index }
+      format.json { render json: @currencies, status: 201 }
+    end
+  end
+
+  def new
+    @currency = Currency.new
+  end
+
+  def create
+    @currency = Currency.new(currency_params)
+    if @currency.save
+      respond_to do |format|
+        format.html { render :index }
+        format.json { render json: @currency, status: 201 }
+      end
+    else
+      render :index
+    end
+  end
+
+  def show
+    
+  end
+
+  def update
+    @currency = Currency.find(params[:id])
+    if @currency.update(currency_params)
+      respond_to do |format|
+        format.html { render :index }
+        format.json { render json: @currency, status: 201 }
+      end
+    else
+      render :index
+    end
+  end
+
+  def destroy
+    @currency = Currency.find(params[:id])
+    if @currency.delete
+      respond_to do |format|
+        format.html { render :index }
+        format.json { render json: @currency, status: 201 }
+      end
+    else
+      render :index
+    end    
+  end
+
+  private
+
+  def currency_params
+    params.require(:currency).permit(:name, :alpha_3_code, :numeric_code, :status)
+  end  
+
 end
