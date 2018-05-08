791bcbc53d8b72a864a8488e627444d9e7c67ddf
Hoon Lee <yl.hoony@gmail.com>
2018-05-08 12:13:41 -0700

    set routes and add add logics for index and new routes

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,2 +1,41 @@
 class AccountsController < ApplicationController
+
+  def index
+    @accounts = current_user.accounts.all
+    respond_to do |format|
+      format.html { render :index }
+      format.json { render json: @accounts, status: 201 }
+    end
+  end
+
+  def new
+    @account = Account.new
+  end
+
+  def create
+    
+  end
+
+  def show
+    
+  end
+
+  def edit
+    
+  end
+
+  def update
+    
+  end
+
+  def destroy
+    
+  end
+
+  private
+
+  def account_params
+    
+  end
+
 end
