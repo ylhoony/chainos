aabc7b6d9b12e423cae8f061c84820b770764f3f
Hoon Lee <yl.hoony@gmail.com>
2018-05-08 20:34:29 -0700

    added ajax to get countries and currencies on document loading

--- app/assets/javascripts/accounts.js
+++ app/assets/javascripts/accounts.js
@@ -1,3 +1,36 @@
-# Place all the behaviors and hooks related to the matching controller here.
-# All this logic will automatically be available in application.js.
-# You can use CoffeeScript in this file: http://coffeescript.org/
+const getCountries = () => {
+  $.ajax({
+    method: "get",
+    url: "/countries/active",
+    dataType: "json"
+  })
+    .done(res => displayCountries(res))
+    .fail(err => console.log(err))
+}
+
+const getCurrencies = () => {
+  $.ajax({
+    method: "get",
+    url: "/currencies/active",
+    dataType: "json"
+  })
+    .done(res => displayCurrencies(res))
+    .fail(err => console.log(err))
+}
+
+const displayCurrencies = (data) => {
+  console.log("displayCurrencies", data);
+}
+
+const displayCountries = (data) => {
+  console.log("displayCountries", data);
+}
+
+const init = () => {
+  getCountries();
+  getCurrencies();
+}
+
+document.addEventListener("DOMContentLoaded", function(event) {
+  init();
+});
