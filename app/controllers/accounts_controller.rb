175e2d6875b330c11c29b2f540b2ad913de8d40d
Hoon Lee <yl.hoony@gmail.com>
2018-05-09 12:46:41 -0700

    set create logic and set_account method

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,6 +1,8 @@
 class AccountsController < ApplicationController
+  before_action :set_account, only: [:show, :edit, :update, :destroy]
 
   def index
+    binding.pry
     @accounts = current_user.accounts.all
     respond_to do |format|
       format.html { render :index }
@@ -13,19 +15,31 @@ class AccountsController < ApplicationController
   end
 
   def create
-    
+    @account = Account.new(account_params)
+    if @account.save
+      @account.employees.create(user_id: current_user.id, account_id: @account.id)
+      respond_to do |format|
+        format.html { render :index }
+        format.json { render json: @accounts, status: 201 }
+      end
+    else
+      respond_to do |format|
+        format.html { render :new }
+        # format.json { render json: @accounts, status: 201 }
+      end
+    end
   end
 
   def show
-    
+
   end
 
   def edit
-    
+
   end
 
   def update
-    
+
   end
 
   def destroy
@@ -34,8 +48,12 @@ class AccountsController < ApplicationController
 
   private
 
+  def set_account
+    @account = current_user.accounts.find_by(id: params[:id])
+  end
+
   def account_params
-    
+    params.require(:account).permit(:name, :country_id, :currency_id, :status)
   end
 
 end
