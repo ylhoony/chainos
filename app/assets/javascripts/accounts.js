fb97a3adbb5f3cfc30676e17df2ca716339f0b7c
Hoon Lee <yl.hoony@gmail.com>
2018-05-11 16:30:29 -0700

    commented out old code, and added new for index page list loading

--- app/assets/javascripts/accounts.js
+++ app/assets/javascripts/accounts.js
@@ -1,66 +1,102 @@
-const initForm = () => {
-  let data = new Object;
+// const initForm = () => {
+//   let data = new Object;
 
-  const formTemplate = document.getElementById("account-form-template").innerHTML;
-  const template = Handlebars.compile(formTemplate);
+//   const formTemplate = document.getElementById("account-form-template").innerHTML;
+//   const template = Handlebars.compile(formTemplate);
 
-  // need to optimize - not sure how to find out 
-  // when it is getting all the data in to data object
-  $.ajax({
-    method: "get",
-    url: "/countries/active",
-    dataType: "json"
-  })
-    .done((res) => {
-      data.countries = res;
-      document.getElementById("content-body").innerHTML = template(data);
-    })
-    .fail(err => console.log(err))
+//   // need to optimize - not sure how to find out 
+//   // when it is getting all the data in to data object
+//   $.ajax({
+//     method: "get",
+//     url: "/countries/active",
+//     dataType: "json"
+//   })
+//     .done((res) => {
+//       data.countries = res;
+//       document.getElementById("content-body").innerHTML = template(data);
+//     })
+//     .fail(err => console.log(err))
 
-  $.ajax({
-    method: "get",
-    url: "/currencies/active",
-    dataType: "json"
-  })
-    .done((res) => {
-      data.currencies = res;
-      document.getElementById("content-body").innerHTML = template(data);
-    })
-    .fail(err => console.log(err))
-  /////
+//   $.ajax({
+//     method: "get",
+//     url: "/currencies/active",
+//     dataType: "json"
+//   })
+//     .done((res) => {
+//       data.currencies = res;
+//       document.getElementById("content-body").innerHTML = template(data);
+//     })
+//     .fail(err => console.log(err))
+//   /////
   
-  $("form#new_account").submit(() => {
-    const $form = $(this);
-    const action = $form.attr("action");
-    const params = $form.serialize();
+//   $("form#new_account").submit(() => {
+//     const $form = $(this);
+//     const action = $form.attr("action");
+//     const params = $form.serialize();
 
-    $.ajax({
-      method: "post",
-      url: action,
-      data: params,
-      dataType: "json"
-    })
-      .done((res) => {
-        console.log(res);
-        // document.location.href = '/accounts'
-      })
-      .fail((err) => {
-        console.log(err);
-      })
+//     $.ajax({
+//       method: "post",
+//       url: action,
+//       data: params,
+//       dataType: "json"
+//     })
+//       .done((res) => {
+//         console.log(res);
+//         // document.location.href = '/accounts'
+//       })
+//       .fail((err) => {
+//         console.log(err);
+//       })
+//   })
+// }
+
+// const handlebarsSetup = () => {
+//   Handlebars.registerPartial('accountFormPartial', document.getElementById("account-form-partial").innerHTML)
+
+//   Handlebars.registerHelper('displayOption', function(item) {
+//     return new Handlebars.SafeString(`<option value="${item.id}">${item.name}</option>`)
+//   })
+// }
+
+// const init = () => {
+//   handlebarsSetup();
+//   initForm();
+// }
+
+// document.addEventListener("DOMContentLoaded", function(event) {
+//   init();
+// });
+
+// // // // // //
+
+
+
+const initIndex = () => {
+  let data = new Object;
+  const source = document.getElementById("account-list-template").innerHTML;
+  const template = Handlebars.compile(source);
+
+  $.ajax({
+    method: "get",
+    url: "/accounts",
+    dataType: "json"
   })
+    .done((res) => {
+      data.accounts = res;
+      $("#content-table").html(template(data));
+    })
+    .fail((err) => {
+      console.log(err);
+    })
 }
 
 const handlebarsSetup = () => {
-  Handlebars.registerPartial('accountFormPartial', document.getElementById("account-form-partial").innerHTML)
-
-  Handlebars.registerHelper('displayOption', function(item) {
-    return new Handlebars.SafeString(`<option value="${item.id}">${item.name}</option>`)
-  })
+  Handlebars.registerPartial("accountListPartial", document.getElementById("account-list-partial").innerHTML);
 }
 
 const init = () => {
   handlebarsSetup();
-  initForm();
+  initIndex();
 }
 
 document.addEventListener("DOMContentLoaded", function(event) {
