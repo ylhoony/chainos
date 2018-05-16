2c2781b07c35675b3dcee90613802755541d3335
Hoon Lee <yl.hoony@gmail.com>
2018-05-16 10:00:19 -0700

    created routes method

--- app/controllers/payment_options_controller.rb
+++ app/controllers/payment_options_controller.rb
@@ -1,2 +1,29 @@
 class PaymentOptionsController < ApplicationController
+
+  def index
+
+  end
+
+  def new
+    
+  end
+
+  def create
+    
+  end
+
+  def edit
+    
+  end
+
+  def update
+    
+  end
+
+  private
+
+  def payment_option_params
+    
+  end
+
 end
