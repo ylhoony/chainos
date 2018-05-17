412f56bba3fead64b72d150d8e1a9b0632fa7430
Hoon Lee <yl.hoony@gmail.com>
2018-05-17 16:10:03 -0700

    added routes methods and set logic for index route

--- app/controllers/payment_terms_controller.rb
+++ app/controllers/payment_terms_controller.rb
@@ -1,2 +1,45 @@
 class PaymentTermsController < ApplicationController
+
+  def index
+    @payment_terms = PaymentTerm.all
+    respond_to do |format|
+      format.html { render :index }
+      format.json { render json: @payment_terms, status: 201 }
+    end
+  end
+
+  def new
+    
+  end
+
+  def create
+    
+  end
+
+  def show
+    
+  end
+
+  def edit
+    
+  end
+
+  def update
+    
+  end
+
+  def destroy
+    
+  end
+
+  private
+
+  def set_payment_term
+    
+  end
+
+  def payment_term_params
+    
+  end
+
 end
