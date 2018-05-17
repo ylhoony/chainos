2fd7fda797a2a7931f9dba96cf471b664b9fa016
Hoon Lee <yl.hoony@gmail.com>
2018-05-17 15:43:55 -0700

    updated PaymentTerm migration file

--- db/migrate/20180517223700_create_payment_terms.rb
+++ db/migrate/20180517223700_create_payment_terms.rb
@@ -2,9 +2,14 @@ class CreatePaymentTerms < ActiveRecord::Migration[5.1]
   def change
     create_table :payment_terms do |t|
       t.integer :account_id
+      t.integer :payment_option_id
       t.string :name
+      t.float :trade_credit_rate
+      t.integer :trade_credit_days
+      t.integer :days
+      t.boolean :status
 
-      t.timestamps
+      t.timestamps null: false
     end
   end
 end
