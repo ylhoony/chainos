b8315419f7aae0c466606819df1c543a080a12c6
Hoon Lee <yl.hoony@gmail.com>
2018-05-16 17:41:50 -0700

    added js for new payment post

--- app/assets/javascripts/payment_options.js
+++ app/assets/javascripts/payment_options.js
@@ -2,6 +2,28 @@ const initForm = () => {
   const source = document.getElementById("payment-option-form-template").innerHTML;
   const template = Handlebars.compile(source);
   $("#content-main").html(template());
+
+  $("form#new_payment_option").on("submit", (e) => {
+    e.preventDefault();
+    const $form = $(e.target);
+    const action = $form.attr("action");
+    const params = $form.serialize();
+
+    $.ajax({
+      method: "post",
+      url: action,
+      dataType: 'json',
+      data: params
+    })
+      .done((res) => {
+        console.log(res);
+        // debugger;
+        window.location.href = `/payment_options/${res.id}`;
+      })
+      .fail((err) => {
+        console.error(err);
+      })
+  });
 }
 
 const init = () => {
