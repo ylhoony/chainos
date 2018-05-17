71acc0ec4d681e773ab86985068c9a5c8ff863b0
Hoon Lee <yl.hoony@gmail.com>
2018-05-17 12:41:01 -0700

    updated initForm function to display edit form filled out

--- app/assets/javascripts/payment_options.js
+++ app/assets/javascripts/payment_options.js
@@ -24,7 +24,47 @@ const initIndex = () => {
 const initForm = () => {
   const source = document.getElementById("payment-option-form-template").innerHTML;
   const template = Handlebars.compile(source);
-  $("#content-main").html(template());
+
+  const pathname = window.location.pathname;
+  if ((pathname === "/payment_options/new") || (pathname === "/payment_options/new/")) {
+    $("#content-main").html(template());
+  } else {
+    let data = new Object;
+    $.ajax({
+      method: "get",
+      url: pathname,
+      dataType: "json"
+    })
+      .done((res) => {
+        $("#content-main").html(template(res));
+
+        $("form#payment_option").on("submit", (e) => {
+          e.preventDefault();
+          // console.log("test");
+
+          const $form = $(e.target);
+          const action = $form.attr("action");
+          const params = $form.serialize();
+
+          $.ajax({
+            method: "put",
+            url: action,
+            dataType: 'json',
+            data: params
+          })
+            .done((res) => {
+              window.location.href = `/payment_options/${res.id}`;
+            })
+            .fail((err) => {
+              console.error(err);
+            });
+        })
+
+      })
+      .fail((err) => {
+        console.log(err);
+      });
+  }
 
   $("form#new_payment_option").on("submit", (e) => {
     e.preventDefault();
@@ -43,18 +83,19 @@ const initForm = () => {
       })
       .fail((err) => {
         console.error(err);
-      })
+      });
   });
+
 }
 
 const init = () => {
   const pathname = window.location.pathname;
   if ((pathname === "/payment_options") || (pathname === "/payment_options/")) {
     initIndex();
-  } else if ((pathname === "/payment_options/new") || (pathname === "/payment_options/new/")) {
+  } else if (pathname.slice(-4).includes("new") || pathname.slice(-5).includes("edit")) {
     initForm();
   } else {
-
+    initShow();
   }
 
   $("a").on("click", (e) => {
