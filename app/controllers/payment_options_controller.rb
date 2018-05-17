d16faed57bd845f6e764279af172d0ce23fad591
Hoon Lee <yl.hoony@gmail.com>
2018-05-17 12:38:49 -0700

    set logic for show, and update routes, and added set_payment_option method for before_action

--- app/controllers/payment_options_controller.rb
+++ app/controllers/payment_options_controller.rb
@@ -1,5 +1,5 @@
 class PaymentOptionsController < ApplicationController
-
+  before_action :set_payment_option, only: [:show, :edit]
   def index
     @payment_options = PaymentOption.all
     respond_to do |format|
@@ -16,7 +16,6 @@ class PaymentOptionsController < ApplicationController
     @payment_option = PaymentOption.new(payment_option_params)
     if @payment_option.save
       respond_to do |format|
-        # format.html { redirect_to payment_option_path(@payment_option) }
         format.json { render json: @payment_option, status: 201 }
       end
     else
@@ -26,9 +25,18 @@ class PaymentOptionsController < ApplicationController
       end
     end
   end
+  def show
+    respond_to do |format|
+      format.html { render :show }
+      format.json { render json: @payment_option, status: 201 }
+    end
+  end
 
   def edit
-    
+    respond_to do |format|
+      format.html { render :edit }
+      format.json { render json: @payment_option, status: 201 }
+    end
   end
 
   def update
@@ -36,6 +44,10 @@ class PaymentOptionsController < ApplicationController
   end
 
   private
+
+  def set_payment_option
+    @payment_option = PaymentOption.find(params[:id])
+  end
 
   def payment_option_params
     params.require(:payment_option).permit(:name, :status)
