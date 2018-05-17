68b656d21459aece55506f87b5713e5cac292e2c
Hoon Lee <yl.hoony@gmail.com>
2018-05-16 17:41:20 -0700

    added js for new payment post

--- app/assets/javascripts/payment_options.js
+++ app/assets/javascripts/payment_options.js
@@ -1,3 +1,14 @@
-# Place all the behaviors and hooks related to the matching controller here.
-# All this logic will automatically be available in application.js.
-# You can use CoffeeScript in this file: http://coffeescript.org/
+const initForm = () => {
+  const source = document.getElementById("payment-option-form-template").innerHTML;
+  const template = Handlebars.compile(source);
+  $("#content-main").html(template());
+}
+
+const init = () => {
+  // console.log("test")
+  initForm();
+}
+
+$(() => {
+  init();
+});
