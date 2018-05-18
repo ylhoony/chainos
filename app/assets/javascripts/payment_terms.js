fd75f68e04a27f450e09a867ac41b219f80a68c3
Hoon Lee <yl.hoony@gmail.com>
2018-05-17 18:16:28 -0700

    set the index page load

---
+++ app/assets/javascripts/payment_terms.js
@@ -0,0 +1,26 @@
+const initIndex = () => {
+  const source = document.getElementById("payment-terms-index-template").innerHTML;
+  const template = Handlebars.compile(source);
+  
+  $.ajax({
+    method: "get",
+    url: "/payment_terms",
+    dataType: "json"
+  })
+    .done((res) => {
+      let data = new Object;
+      data.paymentTerms = res;
+      $("#content-main").html(template(data));
+    })
+    .fail((err) => {
+      console.log(err);
+    });
+}
+
+const init = () => {
+  initIndex();
+}
+
+$(() => {
+  init();
+})
