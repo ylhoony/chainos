eef0580e85cf105da0bcc977e9cf8b631d6e6c96
Hoon Lee <yl.hoony@gmail.com>
2018-05-21 14:41:29 -0700

    added route methods

--- app/controllers/freight_terms_controller.rb
+++ app/controllers/freight_terms_controller.rb
@@ -1,2 +1,41 @@
 class FreightTermsController < ApplicationController
+
+  def index
+    
+  end
+
+  def new
+    
+  end
+
+  def create
+    
+  end
+
+  def show
+    
+  end
+
+  def edit
+    
+  end
+
+  def update
+    
+  end
+
+  def destroy
+    
+  end
+
+  private
+
+  def set_freight_term
+    
+  end
+
+  def freight_term_params
+    
+  end
+
 end
