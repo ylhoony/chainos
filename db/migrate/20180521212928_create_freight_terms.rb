4bed79470df0df6d4b4f90da4b9eca8cc960cbfb
Hoon Lee <yl.hoony@gmail.com>
2018-05-21 14:33:45 -0700

    migrated freight_terms

--- db/migrate/20180521212928_create_freight_terms.rb
+++ db/migrate/20180521212928_create_freight_terms.rb
@@ -1,9 +1,11 @@
 class CreateFreightTerms < ActiveRecord::Migration[5.1]
   def change
     create_table :freight_terms do |t|
+      t.integer :account_id
       t.string :name
+      t.boolean :status, default: false
 
-      t.timestamps
+      t.timestamps null: false
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180517223700) do
+ActiveRecord::Schema.define(version: 20180521212928) do
 
   create_table "accounts", force: :cascade do |t|
     t.string "name"
@@ -43,6 +43,14 @@ ActiveRecord::Schema.define(version: 20180517223700) do
   create_table "employees", force: :cascade do |t|
     t.integer "user_id"
     t.integer "account_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "freight_terms", force: :cascade do |t|
+    t.integer "account_id"
+    t.string "name"
+    t.boolean "status", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
