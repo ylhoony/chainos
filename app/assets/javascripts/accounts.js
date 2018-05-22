f3f88ad1ad4d6845d04d31cfd27404ec658981bd
Hoon Lee <yl.hoony@gmail.com>
2018-05-22 11:36:12 -0700

    changed order and created ajaxData function to do multiple ajax loading

--- app/assets/javascripts/accounts.js
+++ app/assets/javascripts/accounts.js
@@ -1,18 +1,38 @@
+"use strict";
+
+$(document).on('turbolinks:load', () => {
+  init();
+});
+
+const init = () => {
+  const pathname = window.location.pathname;
+  
+  if (pathname.endsWith("/accounts") || pathname.endsWith("/accounts/")) {
+    handlebarsIndexSetup();
+    initIndex();
+  } else if (pathname.slice(-4).includes("new") || pathname.slice(-5).includes("edit")) {
+    handlebarsFormSetup();
+    initForm();
+  } else {
+    initShow();
+  }
+}
+
+const handlebarsIndexSetup = () => {
+  Handlebars.registerPartial("accountListPartial", document.getElementById("account-list-partial").innerHTML);
+}
+
 const initIndex = () => {
   let data = new Object;
   const source = document.getElementById("account-list-template").innerHTML;
   const template = Handlebars.compile(source);
 
-  $.ajax({
-    method: "get",
-    url: "/accounts",
-    dataType: "json"
-  })
+  ajaxData("get", "/accounts", {})
     .done((res) => {
       data.accounts = res;
       $("#content-main").html(template(data));
 
-      $(".to-dashboard").on("click", (e) => {
+      $(".account-change").on("click", (e) => {
         e.preventDefault();
         const action = $(e.target).attr("href");
         $.ajax({
@@ -33,80 +53,62 @@ const initIndex = () => {
     })
 }
 
-const attachEvents = () => {
-  $(".create button").on("click", () => {
-    let data = new Object;
-    const source = document.getElementById("account-form-template").innerHTML;
-    const template = Handlebars.compile(source);
-
-    $.ajax({
-      method: "get",
-      url: "countries/active",
-      dataType: "json"
-    })
-      .done((res) => {
-        data.countries = res;
-        $("#content-main").html(template(data));
-
-        $("form#new_account").on("submit", (e)=> {
-          e.preventDefault();
-          const $form = $(e.target);
-          const action = $form.attr("action");
-          const params = $form.serialize();
-          $.ajax({
-            url: action,
-            method: "post",
-            data: params,
-            dataType: "json"
-          })
-          .done((res) => {
-            window.location.href = "/dashboard"
-          })
-          .fail((err) => {
-            console.error(err);
-          });
-        });
-      })
-      .fail((err) => {
-        console.log(err);
-      });
-
-    $.ajax({
-      method: "get",
-      url: "currencies/active",
-      dataType: "json"
-    })
-      .done((res) => {
-        data.currencies = res;
-        // $("#content-main").html(template(data));
-      })
-      .fail((err) => {
-        console.log(err);
-      });
-  });
-}
-
-const handlebarsIndexSetup = () => {
-  Handlebars.registerPartial("accountListPartial", document.getElementById("account-list-partial").innerHTML);
+const handlebarsFormSetup = () => {
   Handlebars.registerHelper("displayOptions", (item) => {
     return new Handlebars.SafeString(`<option value="${item.id}">${item.name}</option>`);
   })
 }
 
-const init = () => {
-  if (($.ajaxSettings.url).endsWith("/accounts") || ($.ajaxSettings.url).endsWith("/accounts/")) {
-    handlebarsIndexSetup();
-    initIndex();
-  } else {
-    consoel.log("show!")
-  }
+const initForm = () => {
+  let data = new Object;
+  const source = document.getElementById("account-form-template").innerHTML;
+  const template = Handlebars.compile(source);
+
+  const getCountries = ajaxData("get", "/countries/active", {}),
+        getCurrencies = ajaxData("get", "/currencies/active", {})
+
+  $.when(getCountries, getCurrencies)
+    .done((countries, currencies) => {
+      let data = new Object;
+      data.countries = countries[0];
+      data.currencies = currencies[0];
+      
+      $("#content-main").html(template(data));
+
+      $("form#new_account").on("submit", createNewAccount);
+    })
+    .fail((errCountries, errCurrencies) => {
+      // Update with better way to handle
+      console.log(errCountries);
+      console.log(errCurrencies);
+    });
 }
 
-// document.addEventListener("DOMContentLoaded", function(event) {
-//   init();
-// });
+const createNewAccount = (e) => {
+  e.preventDefault();
+  const $form = $(e.target);
+  const action = $form.attr("action");
+  const params = $form.serialize();
 
-$(() => {
-  init();
-  attachEvents();
-})
+  ajaxData("post", action, params)
+    .done((res) => {
+      window.location.href = "/dashboard"
+    })
+    .fail((err) => {
+      console.error(err);
+    });
+}
+
+const initShow = () => {
+  // Update when creating setting page or it is now needed
+  console.log("initShow!");
+}
+
+const ajaxData = (method, dataURL, params) => {
+  return $.ajax({
+    method: method,
+    url: dataURL,
+    data: params,
+    dataType: "json"
+  });
+}
