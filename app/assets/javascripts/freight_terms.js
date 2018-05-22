90386e8d5b897f8cda3f81d32e06015a557b18fa
Hoon Lee <yl.hoony@gmail.com>
2018-05-21 17:38:17 -0700

    added feedContentMain, anchorClickEvent, getData methods

--- app/assets/javascripts/freight_terms.js
+++ app/assets/javascripts/freight_terms.js
@@ -4,6 +4,18 @@ const windowLocationHref = (path) => {
   return window.location.href = path;
 }
 
+const feedContentMain = (template, data) => {
+  $("#content-main").html(template(data));
+  anchorClickEvent();
+}
+
+const anchorClickEvent = () => {
+  $("a").on("click", (e) => {
+    e.preventDefault();
+    window.location.href =  $(e.target).attr("href");
+  });
+}
+
 const postData = (action, params, controller, postAction) => {
   $.ajax({
     method: 'post',
@@ -11,9 +23,21 @@ const postData = (action, params, controller, postAction) => {
     dataType: "json",
     data: params
   }).done((res) => {
-    windowLocationHref(`${controller}/${res.id}`)
+    windowLocationHref(`${controller}/${res.id}`);
   }).fail((err) => {
     console.log("err");
+  })
+}
+
+const getData = (action, template) => {
+  $.ajax({
+    method: "get",
+    url: action,
+    dataType: "json"
+  }).done((res) => {
+    let data = new Object;
+    data.freightTerms = res;
+    feedContentMain(template, data); 
   })
 }
 
@@ -33,28 +57,32 @@ const initForm = () => {
 }
 
 const initIndex = () => {
-  console.log("initIndex");
+  const source = document.getElementById("freight-terms-index-template").innerHTML;
+  const template = Handlebars.compile(source);
+  getData(pathname, template, feedContentMain);
+}
+
+const initShow = () => {
+  const source = document.getElementById("freight-term-show-template").innerHTML;
+  const template = Handlebars.compile(source);
+  
+  getData(pathname, template);
 }
 
 const init = () => {
-  const pathname = window.location.pathname;
   if ((pathname === "/freight_terms") || (pathname === "/freight_terms/")) {
     initIndex();
   } else if (pathname.slice(-4).includes("new") || pathname.slice(-5).includes("edit")) {
     initForm();
-    // HandlebarsFormSetup();
   } else {
-    // initShow();
+    initShow();
   }
 
   $(".create button").on("click", () => {
     window.location.href = "/freight_terms/new"
   });
 
-  $("a").on("click", (e) => {
-    e.preventDefault();
-    window.location.href =  $(e.target).attr("href");
-  });
+  anchorClick();
 }
 
 $(() => {
