53c265c5f21ce66c30047dcf328ec5b5af9c53f9
Hoon Lee <yl.hoony@gmail.com>
2018-05-22 20:13:23 -0700

    added logic for create, edit, delete button

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,11 +1,40 @@
 "use strict";
 
-$(() => {
+$(document).on('turbolinks:load', () => {
   attachEventListener();
-})
+});
 
 const attachEventListener = () => {
-  $(".create button").on("click", () => {
-    window.location.href = `${window.location.pathname}/new`
-  })
+  const pathname = window.location.pathname;
+
+  $("div.create").on("click", () => {
+    if (pathname.endsWith("/"))
+      window.location.href = `${window.location.pathname}new`
+    else {
+      window.location.href = `${window.location.pathname}/new`
+    }
+  });
+
+  $("div.edit").on("click", () => {
+    if (pathname.endsWith("/"))
+      window.location.href = `${window.location.pathname}edit`
+    else {
+      window.location.href = `${window.location.pathname}/edit`
+    }
+  });
+
+  $("div.delete").on("click", () => {
+    $.ajax({
+      method: "delete",
+      url: window.location.pathname,
+      dataType: "json"
+    })
+      .done((res) => {
+        window.location.href = pathname.slice(0, pathname.indexOf("/",1));
+      })
+      .fail((err) => {
+        console.log(err);
+      })
+  });
+
 }
