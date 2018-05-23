00202535dc15364520ac0bd92eb58e45098b52e5
Hoon Lee <yl.hoony@gmail.com>
2018-05-22 19:13:10 -0700

    added btn-sqr-lg

--- app/assets/javascripts/accounts.js
+++ app/assets/javascripts/accounts.js
@@ -8,9 +8,8 @@ const init = () => {
   const pathname = window.location.pathname;
   
   if (pathname.endsWith("/accounts") || pathname.endsWith("/accounts/")) {
-    const createBtn = '<div class="create"><button><i class="far fa-plus fa-lg"></i></button></div>';
+    const createBtn = '<div class="create"><button class="btn-sqr-lg"><i class="far fa-plus fa-lg"></i></button></div>';
     $("#content-header").append(createBtn);
-    $("#content-footer").append(createBtn);
     handlebarsIndexSetup();
     initIndex();
   } else if (pathname.slice(-4).includes("new") || pathname.slice(-5).includes("edit")) {
