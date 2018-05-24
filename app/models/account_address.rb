7bba77bf8be06e681353aa334704de754f40b408
Hoon Lee <yl.hoony@gmail.com>
2018-05-24 02:21:17 -0700

    set model association for country and account_address

--- app/models/account_address.rb
+++ app/models/account_address.rb
@@ -1,3 +1,4 @@
 class AccountAddress < ApplicationRecord
   belongs_to :account
+  belongs_to :country
 end
--- app/models/country.rb
+++ app/models/country.rb
@@ -1,5 +1,6 @@
 class Country < ApplicationRecord
   has_many :accounts
+  has_many :account_addresses
 
   def self.active_countries
     where(status: true)
