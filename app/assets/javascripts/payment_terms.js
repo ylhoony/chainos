6216f0acf36d99ff6d91286f1874fc1346acd226
Hoon Lee <yl.hoony@gmail.com>
2018-05-23 18:17:52 -0700

    changed order and refactored entire code

--- app/assets/javascripts/payment_terms.js
+++ app/assets/javascripts/payment_terms.js
@@ -1,45 +1,50 @@
 "use strict";
 
+$(document).on('turbolinks:load', () => {
+  init();
+});
+
+const init = () => {
+  const pathname = window.location.pathname;
+  if (pathname.endsWith("/payment_terms") || pathname.endsWith("/payment_terms/")) {
+    $("div.edit").remove();
+    $("div.delete").remove();
+    initIndex();
+  } else if (pathname.slice(-4).includes("new") || pathname.slice(-5).includes("edit")) {
+    $("div.create").remove();
+    $("div.edit").remove();
+    $("div.delete").remove();
+    HandlebarsFormSetup();
+    initForm();    
+  } else {
+    $("div.create").remove();
+    initShow();
+  }
+}
+
 const initIndex = () => {
   const source = document.getElementById("payment-terms-index-template").innerHTML;
   const template = Handlebars.compile(source);
   
-  $.ajax({
-    method: "get",
-    url: "/payment_terms",
-    dataType: "json"
-  })
+  ajaxData("get", "/payment_terms", {})
     .done((res) => {
       let data = new Object;
       data.paymentTerms = res;
       $("#content-main").html(template(data));
-
-      $("a").on("click", (e) => {
-        window.location.href = $(e.target).attr("href");
-      })
     })
     .fail((err) => {
       console.log(err);
     });
 }
 
-const initShow = () => {
-  const source = document.getElementById("payment-term-show-template").innerHTML;
-  const template = Handlebars.compile(source);
-  const pathname = window.location.pathname;
-
-  $.ajax({
-    method: "get",
-    url: pathname,
-    dataType: "json"
+const HandlebarsFormSetup = () => {
+  Handlebars.registerHelper("displayOption", (item) => {
+    if (item.selected) {
+      return new Handlebars.SafeString(`<option value="${item.id}" selected>${item.name}</option>`)
+    } else {
+      return new Handlebars.SafeString(`<option value="${item.id}">${item.name}</option>`)
+    }
   })
-    .done((res) => {
-      $("#content-main").html(template(res));
-    })
-    .fail((err) => {
-      console.log(err);
-    });
-
 }
 
 const initForm = () => {
@@ -48,85 +53,72 @@ const initForm = () => {
   const template = Handlebars.compile(source);  
   
   if ((pathname === "/payment_terms/new") || (pathname === "/payment_terms/new/")) {
-    $.ajax({
-      method: "get",
-      url: "/payment_options",
-      dataType: "json"
-    })
+
+    ajaxData("get", "/payment_options", {})
       .done((res) => {
         let data = new Object;
         data.paymentOptions = res;
         $("#content-main").html(template(data));
-
-        $("form#new_payment_term").on("submit", (e) => {
-          e.preventDefault();
-
-          const $form = $(e.target);
-          const action = $form.attr("action")
-          const params = $form.serialize();
-
-          $.ajax({
-            method: "post",
-            url: action,
-            dataType: "json",
-            data: params
-          })
-            .done((res) => {
-              window.location.href = `/payment_terms/${res.id}`;
-            })
-            .fail((err) => {
-              console.log(err);
-            });
-
-        });
+        $("form#new_payment_term").on("submit", submitPaymentTermForm);
       })
       .fail((err) => {
         console.log(err);
       });
   } else {
-    $.ajax({
-      method: "get",
-      url: pathname,
-      dataType: "json"
-    })
-      .done((res) => {
-        $("#content-main").html(template(res));
-        
+    const getPaymentTerm = ajaxData("get", pathname, {}),
+          getPaymentOptions = ajaxData("get", "/payment_options", {})
+    $.when(getPaymentTerm, getPaymentOptions)
+      .done((paymentTermData, paymentOptionsList) => {
+        let data = new Object;
+        data.paymentTerm = paymentTermData[0];
+        data.paymentOptions = paymentOptionsList[0];
+        const optionId = data.paymentTerm.payment_option.id
+        data.paymentOptions.map((e) => {
+          if (e.id == optionId) { e.selected = true; }
+        });
+        $("#content-main").html(template(data));
       })
       .fail((err) => {
         console.log("err", err);
       })
-    
   }
 }
 
-const HandlebarsFormSetup = () => {
-  Handlebars.registerHelper("displayOption", (item) => {
-    return new Handlebars.SafeString(`<option value="${item.id}">${item.name}</option>`)
-  })
+const submitPaymentTermForm = (e) => {
+  e.preventDefault();
+  const $form = $(e.target);
+  const method = $form.attr("method");
+  const action = $form.attr("action");
+  const params = $form.serialize();
+
+  ajaxData(method, action, params)
+    .done((res) => {
+      window.location.href = `/payment_terms/${res.id}`;
+    })
+    .fail((err) => {
+      console.log(err);
+    });
 }
 
-const init = () => {
+const initShow = () => {
   const pathname = window.location.pathname;
-  if ((pathname === "/payment_terms") || (pathname === "/payment_terms/")) {
-    initIndex();
-  } else if (pathname.slice(-4).includes("new") || pathname.slice(-5).includes("edit")) {
-    initForm();
-    HandlebarsFormSetup();
-  } else {
-    initShow();
-  }
+  const source = document.getElementById("payment-term-show-template").innerHTML;
+  const template = Handlebars.compile(source);
 
-  $(".create button").on("click", () => {
-    window.location.href = "/payment_terms/new"
-  });
-
-  $("a").on("click", (e) => {
-    e.preventDefault();
-    window.location.href =  $(e.target).attr("href");
-  });
+  ajaxData("get", pathname, {})
+    .done((res) => {
+      $("#content-main").html(template(res));
+    })
+    .fail((err) => {
+      console.log(err);
+    });
 }
 
-$(() => {
-  init();
-})
+const ajaxData = (method, dataURL, params) => {
+  return $.ajax({
+    method: method,
+    url: dataURL,
+    data: params,
+    dataType: "json"
+  });
+}
