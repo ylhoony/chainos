14134ff1ddebc188424800d95c662d361bf05867
Hoon Lee <yl.hoony@gmail.com>
2018-05-23 19:05:14 -0700

    added routes in UOM controller file

--- app/controllers/unit_of_measures_controller.rb
+++ app/controllers/unit_of_measures_controller.rb
@@ -1,2 +1,41 @@
 class UnitOfMeasuresController < ApplicationController
+
+
+  def index
+    
+  end
+
+  def new
+    
+  end
+
+  def create
+    
+  end
+
+  def show
+    
+  end
+
+  def edit
+    
+  end
+
+  def update
+    
+  end
+
+  def destroy
+    
+  end
+
+  private
+
+  def set_unit_of_measure
+    
+  end
+
+  def unit_of_measure_params
+    
+  end
 end
