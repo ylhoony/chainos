7d8d87527d58d54aecca48ba60e4ecfd2d4ece49
Hoon Lee <yl.hoony@gmail.com>
2018-05-24 15:58:00 -0700

    updated set_account_address and account_address_params methods

--- app/controllers/account_addresses_controller.rb
+++ app/controllers/account_addresses_controller.rb
@@ -14,7 +14,12 @@ class AccountAddressesController < ApplicationController
   end
 
   def create
-    
+    @account_address = current_account.account_addresses.build(account_address_params)
+    @account_address.save
+    respond_to do |format|
+      format.html { redirect_to account_address_path(@account_address) }
+      format.json { render json: @account_address, status: 201 }
+    end
   end
 
   def show
@@ -36,11 +41,25 @@ class AccountAddressesController < ApplicationController
   private
 
   def set_account_address
-    
+    @account_address = current_account.account_addresses.find(params[:id])
   end
 
   def account_address_params
-    
+    params.require(:account_address).permit(
+      :name, 
+      :contact,
+      :address1,
+      :address2,
+      :city,
+      :state,
+      :country_id,
+      :postal_code,
+      :email,
+      :phone,
+      :fax,
+      :comment,
+      :status
+    )
   end
 
 end
