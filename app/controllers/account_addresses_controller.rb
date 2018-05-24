cc398e7d389b9754be5125e13a0f7324d8ffeb47
Hoon Lee <yl.hoony@gmail.com>
2018-05-24 01:47:44 -0700

    set routes

--- app/controllers/account_addresses_controller.rb
+++ app/controllers/account_addresses_controller.rb
@@ -1,2 +1,41 @@
 class AccountAddressesController < ApplicationController
+
+  def index
+    
+  end
+
+  def new
+    
+  end
+
+  def create
+    
+  end
+
+  def show
+    
+  end
+
+  def edit
+    
+  end
+
+  def update
+    
+  end
+
+  def destroy
+    
+  end
+
+  private
+
+  def set_account_address
+    
+  end
+
+  def account_address_params
+    
+  end
+
 end
