ca131f7e712841ee2bbcb6c3e43c481c567fb874
Hoon Lee <yl.hoony@gmail.com>
2018-05-25 11:15:35 -0700

    opened routes for warehouse

--- app/controllers/warehouses_controller.rb
+++ app/controllers/warehouses_controller.rb
@@ -1,2 +1,41 @@
 class WarehousesController < ApplicationController
+
+  def index
+    
+  end
+
+  def new
+    
+  end
+
+  def create
+    
+  end
+
+  def show
+    
+  end
+
+  def edit
+    
+  end
+  
+  def update
+    
+  end
+
+  def destroy
+    
+  end
+
+  private
+
+  def set_warehouse
+    
+  end
+
+  def warehouse_params
+    
+  end
+
 end
