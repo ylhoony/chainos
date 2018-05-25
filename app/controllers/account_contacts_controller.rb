05ebdfebe7dd74f50a799a8f7fb28958b4dcf99a
Hoon Lee <yl.hoony@gmail.com>
2018-05-24 18:57:02 -0700

    set routes

--- app/controllers/account_contacts_controller.rb
+++ app/controllers/account_contacts_controller.rb
@@ -1,2 +1,42 @@
 class AccountContactsController < ApplicationController
+
+  def index
+    @account_contacts = current_acocunt.account_contacts.all
+    
+  end
+
+  def new
+    
+  end
+
+  def create
+    
+  end
+
+  def show
+    
+  end
+
+  def edit
+    
+  end
+
+  def update
+
+  end
+
+  def destroy
+    
+  end
+
+  private
+
+  def set_account_contact
+    
+  end
+
+  def account_contact_params
+    
+  end
+
 end
