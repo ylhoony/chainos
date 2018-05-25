777a47a54932e99b8925a30496dc71a1d78e3d1f
Hoon Lee <yl.hoony@gmail.com>
2018-05-25 16:44:56 -0700

    added active_warehouses class method

--- app/models/warehouse.rb
+++ app/models/warehouse.rb
@@ -3,4 +3,9 @@ class Warehouse < ApplicationRecord
   belongs_to :country
   has_many :customers, class_name: "Customer"
   has_many :suppliers, class_name: "Supplier"
+
+  def self.active_warehouses(account)
+    account.warehouses.where(status: true)
+  end
+
 end
