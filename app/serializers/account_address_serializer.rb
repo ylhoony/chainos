4c884efb58396b3891d3a07cbf6075b021925e64
Hoon Lee <yl.hoony@gmail.com>
2018-05-25 11:54:21 -0700

    removed comment from warehouse serializer

--- app/serializers/account_address_serializer.rb
+++ app/serializers/account_address_serializer.rb
@@ -1,5 +1,7 @@
 class AccountAddressSerializer < ActiveModel::Serializer
   belongs_to :account
   belongs_to :country
-  attributes :id, :name, :contact, :address1, :address2, :city, :state, :postal_code, :email, :phone, :fax, :comment, :status
+  attributes :id, :name, :contact, :address1, :address2, 
+             :city, :state, :postal_code, :email, :phone,
+             :fax, :comment, :status
 end
--- app/serializers/warehouse_serializer.rb
+++ app/serializers/warehouse_serializer.rb
@@ -2,5 +2,5 @@ class WarehouseSerializer < ActiveModel::Serializer
   belongs_to :country
   attributes :id, :name, :contact, :address1, :address2, 
              :city, :state, :postal_code, :email, :phone,
-             :fax, :comment, :status
+             :fax, :status
 end
