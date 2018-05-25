e8b74562f2476fd1a796a4e945252e70fb1269a2
Hoon Lee <yl.hoony@gmail.com>
2018-05-25 11:36:54 -0700

    set model association for country and warehouse

--- app/models/country.rb
+++ app/models/country.rb
@@ -1,6 +1,7 @@
 class Country < ApplicationRecord
   has_many :accounts
   has_many :account_addresses
+  has_many :warehouses
 
   def self.active_countries
     where(status: true)
--- app/models/warehouse.rb
+++ app/models/warehouse.rb
@@ -1,3 +1,4 @@
 class Warehouse < ApplicationRecord
   belongs_to :account
+  belongs_to :country
 end
