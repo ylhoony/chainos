c6d26f5ad059244c7f3d6fcf6a4fe19de41a23c8
Hoon Lee <yl.hoony@gmail.com>
2018-05-25 15:20:11 -0700

    removed association for companies, and set customers/suppliers associations instead

--- app/models/account.rb
+++ app/models/account.rb
@@ -3,7 +3,8 @@ class Account < ApplicationRecord
   belongs_to :currency
   has_many :account_addresses
   has_many :account_contacts
-  has_many :companies
+  has_many :customers, class_name: "Customer"
+  has_many :suppliers, class_name: "Supplier"
   has_many :employees
   has_many :users, through: :employees
   has_many :freight_terms
