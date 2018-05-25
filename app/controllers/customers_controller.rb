abb2ade38d263e77d959cf034840d55b454a6414
Hoon Lee <yl.hoony@gmail.com>
2018-05-25 14:30:15 -0700

    set the basic routes

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,2 +1,40 @@
 class CustomersController < ApplicationController
+
+  def index
+    
+  end
+
+  def new
+    
+  end
+
+  def create
+    
+  end
+
+  def show
+    
+  end
+
+  def edit
+    
+  end
+
+  def update
+    
+  end
+
+  def destroy
+    
+  end
+
+  private
+
+  def set_customer
+    
+  end
+
+  def customer_params
+    
+  end
 end
