dfed840e8f290a77419c02b33face45768513441
Hoon Lee <yl.hoony@gmail.com>
2018-05-28 12:19:20 -0700

    set edit, destroy, route

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,4 +1,5 @@
 class CustomersController < ApplicationController
+  before_action :set_customer, only: [:show, :edit, :update, :destroy]
 
   def index
     @customers = current_account.customers.all
@@ -14,15 +15,29 @@ class CustomersController < ApplicationController
   end
 
   def create
-    
+    @customer = current_account.customers.build(customer_params)
+    @customer.save
+    respond_to do |format|
+      format.html { redirect_to customer_path(@customer) }
+      format.js
+      format.json { render json: @customer, status: 201 }
+    end
   end
 
   def show
-    
+    respond_to do |format|
+      format.html { render :show }
+      format.js
+      format.json { render json: @customer, status: 201 }
+    end
   end
 
   def edit
-    
+    respond_to do |format|
+      format.html { render :edit }
+      format.js
+      format.json { render json: @customer, status: 201 }
+    end
   end
 
   def update
@@ -30,16 +45,21 @@ class CustomersController < ApplicationController
   end
 
   def destroy
-    
+    @customer.delete
+    respond_to do |format|
+      format.html { redirect_to :index }
+      format.js
+      format.json { render json: @customer, status: 201 }
+    end
   end
 
   private
 
   def set_customer
-    
+    @customer = current_account.customers.find(params[:id])
   end
 
   def customer_params
-    
+    params.require(:customer).permit(:name, :comment, :warehouse_id, :payment_term_id, :currency_id, :status)
   end
 end
