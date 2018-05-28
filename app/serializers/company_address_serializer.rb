6c2a862dc2d5ad304905e9caca0f14d28b51efe3
Hoon Lee <yl.hoony@gmail.com>
2018-05-28 12:43:08 -0700

    generated company_address resource

---
+++ app/serializers/company_address_serializer.rb
@@ -0,0 +1,3 @@
+class CompanyAddressSerializer < ActiveModel::Serializer
+  attributes :id, :name
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :company_addresses
   resources :suppliers
   resources :customers
   
