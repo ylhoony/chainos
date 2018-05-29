75e02b4a58747299abcd01b4f53cc3f08bff7fb1
Hoon Lee <yl.hoony@gmail.com>
2018-05-29 15:42:21 -0700

    opened route methods

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,41 @@
 class ProductsController < ApplicationController
+
+  def index
+    
+  end
+
+  def new
+    
+  end
+
+  def create
+    
+  end
+
+  def show
+    
+  end
+
+  def edit
+    
+  end
+
+  def update
+    
+  end
+
+  def destroy
+    
+  end
+
+  private
+
+  def set_product
+    
+  end
+
+  def product_params
+    
+  end
+
 end
