a2bbf4673790f1feeaec54616a2f612b2085f43f
Hoon Lee <yl.hoony@gmail.com>
2018-05-28 19:15:01 -0700

    opened more fields for company_addresses and set associations

--- app/serializers/company_address_serializer.rb
+++ app/serializers/company_address_serializer.rb
@@ -1,3 +1,7 @@
 class CompanyAddressSerializer < ActiveModel::Serializer
-  attributes :id, :name
+  belongs_to :country
+  attributes :id, :company_id, :name,
+             :contact, :address1, :address2,
+             :city, :state, :postal_code,
+             :email, :phone, :fax, :comment, :status
 end
--- app/serializers/country_serializer.rb
+++ app/serializers/country_serializer.rb
@@ -1,6 +1,7 @@
 class CountrySerializer < ActiveModel::Serializer
   has_many :accounts
   has_many :account_addresses
+  has_many :company_addresses
   has_many :warehouses
   attributes :id, :name, :alpha_2_code, :alpha_3_code, :numeric_code, :status
 end
