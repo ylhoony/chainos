dedb30f727fe6bd11d6710caf1352602639203ff
Hoon Lee <yl.hoony@gmail.com>
2018-05-28 19:50:50 -0700

    set index route for json response

--- app/controllers/company_addresses_controller.rb
+++ app/controllers/company_addresses_controller.rb
@@ -1,2 +1,45 @@
 class CompanyAddressesController < ApplicationController
+
+  def index
+    @company_addresses = CompanyAddress.where(company_id: params[:supplier_id]) if params[:supplier_id]
+    @company_addresses = CompanyAddress.where(company_id: params[:customer_id]) if params[:customer_id]
+    respond_to do |format|
+      format.json { render json: @company_addresses, status: 201 }
+    end
+  end
+
+  def new
+    
+  end
+
+  def create
+    
+  end
+
+  def show
+    binding.pry
+  end
+
+  def edit
+    
+  end
+
+  def update
+    
+  end
+
+  def destroy
+    
+  end
+
+  private
+
+  def set_company_address
+    
+  end
+
+  def company_address_params
+    
+  end
+
 end
