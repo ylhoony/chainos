edd835d7d268ccf221bde450584a37ff7dd2fdb1
Hoon Lee <yl.hoony@gmail.com>
2018-05-29 16:47:55 -0700

    set the logic for product controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,12 @@
 class ProductsController < ApplicationController
+  before_action :set_product, only: [:show, :edit, :update, :destroy]
 
   def index
-    
+    @products = current_account.products.all
+    respond_to do |format|
+      format.html { render :index }
+      format.json { render json: @products, status: 201 }
+    end
   end
 
   def new
@@ -9,33 +14,52 @@ class ProductsController < ApplicationController
   end
 
   def create
-    
+    @product = current_account.products.build(product_params)
+    @product.save
+    respond_to do |format|
+      format.html { redirect_to product_path(@product) }
+      format.json { render json: @product, status: 201 }
+    end
   end
 
   def show
-    
+    respond_to do |format|
+      format.html { render :show }
+      format.json { render json: @product, status: 201 }
+    end
   end
 
   def edit
-    
+    respond_to do |format|
+      format.html { render :edit }
+      format.json { render json: @product, status: 201 }
+    end
   end
 
   def update
-    
+    @product.update(product_params)
+    respond_to do |format|
+      format.html { redirect_to product_path(@product) }
+      format.json { render json: @product, status: 201 }
+    end
   end
 
   def destroy
-    
+    @product.delete
+    respond_to do |format|
+      format.html { redirect_to :index }
+      format.json { render json: @product, status: 201 }
+    end
   end
 
   private
 
   def set_product
-    
+    @product = current_account.products.find(params[:id])
   end
 
   def product_params
-    
+    params.require(:product).permit(:sku, :name, :description, :unit_of_measure_id, :price)
   end
 
 end
