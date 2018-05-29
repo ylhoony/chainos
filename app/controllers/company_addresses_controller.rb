1c5e2d48a81a3b36b5f43f27c54b945d39a22d9b
Hoon Lee <yl.hoony@gmail.com>
2018-05-29 15:06:44 -0700

    set routes logics

--- app/controllers/company_addresses_controller.rb
+++ app/controllers/company_addresses_controller.rb
@@ -1,5 +1,5 @@
 class CompanyAddressesController < ApplicationController
-
+  before_action :set_company_address, only: [:edit, :update, :destroy]
   def index
     @company_addresses = CompanyAddress.where(company_id: params[:supplier_id]) if params[:supplier_id]
     @company_addresses = CompanyAddress.where(company_id: params[:customer_id]) if params[:customer_id]
@@ -13,33 +13,61 @@ class CompanyAddressesController < ApplicationController
   end
 
   def create
-    
+    @company_address = Supplier.find(params[:supplier_id]).company_addresses.build(company_address_params) if params[:supplier_id]
+    @company_address = Customer.find(params[:customer_id]).company_addresses.build(company_address_params) if params[:customer_id]
+    @company_address.save
+    respond_to do |format|
+      format.json { render json: @company_address, status: 201 }
+    end
   end
 
   def show
-    binding.pry
+
   end
 
   def edit
-    
+    respond_to do |format|
+      format.html { render :edit }
+      format.json { render json: @company_address, status: 201 }
+    end
   end
 
   def update
-    
+    @company_address.update(company_address_params)
+    respond_to do |format|
+      format.json { render json: @company_address, status: 201 }
+    end
   end
 
   def destroy
-    
+    @company_address[0].delete
+    respond_to do |format|
+      format.json { render json: @company_address, status: 201 }
+    end
   end
 
   private
 
   def set_company_address
-    
+    @company_address = Supplier.find(params[:supplier_id]).company_addresses.where(id: params[:id]) if params[:supplier_id]
+    @company_address = Customer.find(params[:customer_id]).company_addresses.where(id: params[:id]) if params[:customer_id]
   end
 
   def company_address_params
-    
+    params.require(:company_address).permit(
+      :name, 
+      :contact, 
+      :address1,
+      :address2,
+      :city,
+      :state,
+      :country_id,
+      :postal_code,
+      :email,
+      :phone,
+      :fax,
+      :comment,
+      :status
+    )
   end
-
 end
