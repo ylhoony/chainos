fa75fe144d85e083778ff2ca49d085521ae83d08
Hoon Lee <yl.hoony@gmail.com>
2018-05-30 11:49:56 -0700

    updated inheritance, and opened routes

--- app/controllers/purchase_orders_controller.rb
+++ app/controllers/purchase_orders_controller.rb
@@ -1,2 +1,46 @@
-class PurchaseOrdersController < ApplicationController
+class PurchaseOrdersController < CompanyOrdersController
+
+
+  def index
+    @purchase_orders = current_account.purchase_orders.all
+    respond_to do |format|
+      format.html { render :index }
+      format.json { render json: @purchase_orders, status: 201 }
+    end
+  end
+
+  def new
+
+  end
+
+  def create
+    
+  end
+
+  def show
+    
+  end
+
+  def edit
+    
+  end
+
+  def update
+    
+  end
+
+  def delete
+
+  end
+
+  private
+
+  def set_purchase_order
+    
+  end
+
+  def purchase_order_params
+    
+  end
+
 end
