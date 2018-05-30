11049a3c6e19484da3a07bd443bfc4a0c3567c34
Hoon Lee <yl.hoony@gmail.com>
2018-05-30 11:44:45 -0700

    updated model associtaion

--- app/models/company_order.rb
+++ app/models/company_order.rb
@@ -1,8 +1,10 @@
 class CompanyOrder < ApplicationRecord
-  scope :sales_orders -> { where(type: "SalesOrder") }
-  scope :purchase_orders -> { where(type: "PurchaseOrder") }
+  scope :sales_orders, -> { where(type: "SalesOrder") }
+  scope :purchase_orders, -> { where(type: "PurchaseOrder") }
 
   belongs_to :company
+  belongs_to :company_address
   belongs_to :currency
   belongs_to :warehouse
+
 end
