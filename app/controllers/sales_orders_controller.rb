857d19c001466368e4d2e6404172d1e15c40a86c
Hoon Lee <yl.hoony@gmail.com>
2018-05-30 11:48:20 -0700

    set index route logic

--- app/controllers/sales_orders_controller.rb
+++ app/controllers/sales_orders_controller.rb
@@ -1,2 +1,45 @@
-class SalesOrdersController < ApplicationController
+class SalesOrdersController < CompanyOrdersController
+
+  def index
+    @sales_orders = current_account.sales_orders.all
+    respond_to do |format|
+      format.html { render :index }
+      format.json { render json: @sales_orders, status: 201 }
+    end
+  end
+
+  def new
+
+  end
+
+  def create
+    
+  end
+
+  def show
+    
+  end
+
+  def edit
+    
+  end
+
+  def update
+    
+  end
+
+  def delete
+
+  end
+
+  private
+
+  def set_sales_order
+    
+  end
+
+  def sales_order_params
+    
+  end
+
 end
